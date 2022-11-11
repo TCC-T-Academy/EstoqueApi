86188ab13b56df9f12dd2ecdf54a3dfe7027d678
Eduardo Hartwig <eduardohartwigvieira@gmail.com>
2022-11-11 14:43:25 -0300

    CRUD Reservas

--- src/main/java/com/estoqueapi/EstoqueApi/Repositorios/ReservasRepository.java
+++ src/main/java/com/estoqueapi/EstoqueApi/Repositorios/ReservasRepository.java
@@ -1,4 +1,10 @@
 package com.estoqueapi.EstoqueApi.Repositorios;
 
-public interface ReservasRepository {
+import com.estoqueapi.EstoqueApi.Entidades.Reservas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservasRepository extends JpaRepository<Reservas, Long> {
+
 }
--- src/main/java/com/estoqueapi/EstoqueApi/Servicos/ReservasService.java
+++ src/main/java/com/estoqueapi/EstoqueApi/Servicos/ReservasService.java
@@ -1,4 +1,66 @@
 package com.estoqueapi.EstoqueApi.Servicos;
 
+import com.estoqueapi.EstoqueApi.Entidades.Reservas;
+import com.estoqueapi.EstoqueApi.Repositorios.ReservasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
 public class ReservasService {
+    @Autowired
+    ReservasRepository reservasRepository;
+    public List<Reservas> consultar(){
+        return reservasRepository.findAll();
+    }
+
+    public Reservas consultarById(Long id){
+        Optional<Reservas> obj = reservasRepository.findById(id);
+        Reservas res = null;// obj.orElseThrow(()-> new EntityNotFoundException("Reserva não encontrada"));
+        try{
+            res = obj.get();
+        }
+        catch (NoSuchElementException e){
+            throw new EntityNotFoundException("Reserva não encontrada");
+        }
+        return res;
+    }
+
+    /*public Object consultarByIdItem(Long idItem){
+        List<Reservas> lista = reservasRepository.findReservasByIdItem(idItem);
+        if(lista.isEmpty()){
+            return "Lista Vazia";
+        }
+        return lista;
+    }*/
+
+    @Transactional
+    public Reservas salvar(Reservas reservas){
+        return reservasRepository.save(reservas);
+    }
+
+    public Reservas alterar(Long idreserva, Reservas reservas){
+        Reservas res = this.consultarById(idreserva);
+
+        res.setFinalizada(reservas.isFinalizada());
+        res.setQuantidadeReserva(reservas.getQuantidadeReserva());
+        res.setDataPrevista(reservas.getDataPrevista());
+        res.setOrdem(reservas.getOrdem());
+        res.setUsuarios_idUsuario(reservas.getUsuarios_idUsuario());
+        res.setItens_idItem(reservas.getItens_idItem());
+
+        return this.salvar(res);
+    }
+
+    @Transactional
+    public void excluir(Long idreserva){
+        Reservas reservas = this.consultarById(idreserva);
+        reservasRepository.delete(reservas);
+    }
+
 }
