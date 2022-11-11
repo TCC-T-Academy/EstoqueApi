52f8e1b4792cf3310b938613abe0a3ffb583f041
Eduardo Hartwig <eduardohartwigvieira@gmail.com>
2022-11-11 08:56:26 -0300

    Commit inicial

---
+++ src/main/java/com/estoqueapi/EstoqueApi/Config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.estoqueapi.EstoqueApi.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI estoqueApi() {
+      return new OpenAPI()
+                .info(new Info().title("EstoqueApi")
+                        .description("API REST de gerenciamento de Estoque")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
