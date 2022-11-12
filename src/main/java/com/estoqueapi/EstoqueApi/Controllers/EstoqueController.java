496e2f7d46a10323c56326b4a9de21e3db7b3156
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-12 18:47:54 -0300

    criada a entidade Estoque classes repositorio, service e controler. Criada apenas função listar

--- src/main/java/com/estoqueapi/EstoqueApi/Controllers/EstoqueController.java
+++ src/main/java/com/estoqueapi/EstoqueApi/Controllers/EstoqueController.java
@@ -1,7 +1,27 @@
 package com.estoqueapi.EstoqueApi.Controllers;
 
+import com.estoqueapi.EstoqueApi.Entidades.Estoque;
+import com.estoqueapi.EstoqueApi.Servicos.EstoqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.PrimitiveIterator;
+
 @RestController
+@RequestMapping("estoque")
 public class EstoqueController {
+
+    @Autowired
+    private EstoqueService estoqueService;
+
+    @GetMapping
+    public ResponseEntity<List<Estoque>> consultarEstoque(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(estoqueService.listarEstoque());
+    }
+
 }
--- src/main/java/com/estoqueapi/EstoqueApi/Servicos/EstoqueService.java
+++ src/main/java/com/estoqueapi/EstoqueApi/Servicos/EstoqueService.java
@@ -1,4 +1,18 @@
 package com.estoqueapi.EstoqueApi.Servicos;
 
+import com.estoqueapi.EstoqueApi.Entidades.Estoque;
+import com.estoqueapi.EstoqueApi.Repositorios.EstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EstoqueService {
+
+    @Autowired
+    private EstoqueRepository estoqueRepository;
+    public List<Estoque> listarEstoque(){
+        return estoqueRepository.findAll();
+    }
 }
