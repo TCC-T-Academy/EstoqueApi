72a72d24932b6e43b368e5bb1c0e8b84ed4cc76b
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-16 16:24:22 -0300

    Adicao de consulta de estoque

--- src/main/java/com/estoqueapi/EstoqueApi/Controllers/EstoqueController.java
+++ src/main/java/com/estoqueapi/EstoqueApi/Controllers/EstoqueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class EstoqueController {
     @GetMapping
     public ResponseEntity<List<Estoque>> consultarEstoque(){
         return ResponseEntity.status(HttpStatus.FOUND).body(estoqueService.listarEstoque());
+    }
+
+    @GetMapping("/{idItem}")
+    public ResponseEntity<Estoque> consultarEstoqueIdItem(@PathVariable("idItem") long idItem){
+        return ResponseEntity.status(HttpStatus.FOUND).body(estoqueService.buscarEstoqueIdItem(idItem));
     }
 
 }
