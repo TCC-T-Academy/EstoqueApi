87a529778f97fe9c7638c15a4159af868cc55932
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-25 22:41:21 -0300

    alteracao de atributo para private e renomeacao de exceptions para AcaoNaoPermitidaException

--- src/main/java/com/estoqueapi/EstoqueApi/Servicos/ReservaService.java
+++ src/main/java/com/estoqueapi/EstoqueApi/Servicos/ReservaService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class ReservaService {
     @Autowired
-    ReservaRepository reservaRepository;
+    private ReservaRepository reservaRepository;
     public List<Reserva> consultar(){
         return reservaRepository.findAll();
     }
