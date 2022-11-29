38aa35c1188365b732e9175b34a5403a6f3abd3a
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-29 13:54:40 -0300

    Security: Criada classe de Roles referentes as permissoes dos usuarios.

---
+++ src/main/java/com/estoqueapi/EstoqueApi/Entidades/Role.java
@@ -0,0 +1,40 @@
+package com.estoqueapi.EstoqueApi.Entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRole;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.idRole = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return idRole;
+    }
+
+    public void setId(Long idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+}
